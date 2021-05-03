d62136b051e2507d4d89d7bf572127f972e90338
Felipe Guimaraes <guima.felipec@gmail.com>
2021-05-03 09:43:16 -0300

    changing hungarian to be 0 indexed

--- graphs/hungarian.cpp
+++ graphs/hungarian.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define MAXM 1010
 #define INF 1000000000
 
-//indexar em 1
+// essa versão é indexada em 0
 //fazer n <= m
 // complexidade O(n^2 * m)
 int cost[MAXN][MAXM];
@@ -15,14 +15,14 @@ vector<int> pu, pv, par, way;
 int n, m;
 
 void hungarian(){
-	pu.assign(n+1, 0);
+	pu.assign(n, 0);
 	pv.assign(m+1, 0);
-	way.assign(m+1, 0);
-	par.assign(m+1, 0);
+	way.assign(m+1, m);
+	par.assign(m+1, -1);
 
-	for(int i = 1; i <= n; i++){
-		par[0] = i;
-		int j0 = 0;
+	for(int i = 0; i < n; i++){
+		par[m] = i;
+		int j0 = m;
 		vector<int> minv(m+1, INF);
 		vector<bool> used(m+1, false);
 
@@ -30,7 +30,7 @@ void hungarian(){
 			used[j0] = true;
 			int i0 = par[j0], delta = INF, j1;
 
-			for(int j = 1; j <= m; j++){
+			for(int j = 0; j < m; j++){
 				if(!used[j]){
 					int cur = cost[i0][j] - pu[i0] - pv[j];
 					if(cur < minv[j]){
@@ -54,28 +54,28 @@ void hungarian(){
 			}
 
 			j0 = j1;
-		} while(par[j0] != 0);
+		} while(par[j0] != -1);
 		do{
 			int j1 = way[j0];
 			par[j0] = par[j1];
 			j0 = j1;
-		} while(j0);
+		} while(j0 != m);
 	}
 }
 
 int main(){
 	cin >> n >> m;
-	for(int i = 1; i <= n; i++){
-		for(int j = 1; j <= m; j++){
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < m; j++){
 			cin >> cost[i][j];
 			//cost[i][j] += 3;
 		}
 	}
 
 	hungarian();
-	cout << "total: " << -pv[0] << endl;
-	for(int i = 1; i <= m; i++){
-		if(par[i]){
+	cout << "total cost: " << -pv[m] << endl;
+	for(int i = 0; i < m; i++){
+		if(par[i] != -1){
 			cout << par[i] << " " << i << endl;
 		}
 	}
